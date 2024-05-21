8da70199b0e1d59542a0adfc77a2fbf52c31b566
Edgar Sierra <EdgarSierra.ca@gmail.com>
2024-05-21 12:48:57 -0400

    Second attempt to deploy on Vercel. Change in APP's URLs.

--- app01/urls.py
+++ app01/urls.py
@@ -1,10 +1,13 @@
 from .views import fvHello, fvGreeting, fvNumber, fvInicio, fvVariable, fvConditionIf, fvLoopFor
 from django.urls import path
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
     # path('', fvHello, name = 'vHello'),
     # path('greeting/<str:sPersonName>/', fvGreeting, name = 'vGreeting'),
     # path('number/', fvNumber, name= 'vNumber'),
+    path('',TemplateView.as_view(template_name='index.html'), name='index'),
     path('inicio/', fvInicio, name='vInicio'),
     path('variable/', fvVariable, name= 'vVariable'),
     path('conditional/', fvConditionIf, name= 'vConditionIf'),
