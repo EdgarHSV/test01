056eb23a39cba3d462f9cc88be8726c29214f268
Edgar Sierra <EdgarSierra.ca@gmail.com>
2024-05-22 15:19:48 -0400

    Preparing deploy vercel with video instructions. Step: creatinng the DB.

--- _test01_core/settings.py
+++ _test01_core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%mda7z8v0l%$woi!j&pre2xbnwmjxq_^w3j2_jf26$au$8)bp2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1','localhost','now.sh','vercel.app']
 
 
 # Application definition
@@ -122,10 +122,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static/"),
-)
+STATIC_URL = 'static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static/"),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
